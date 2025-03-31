071c02b2126a274a9c552380ee23bf973c0b9225
leeksh06 <104353105+leeksh06@users.noreply.github.com>
2025-04-01 02:19:39 +0900

    4/1 수정

--- camping/lib/main.dart
+++ camping/lib/main.dart
@@ -3,12 +3,15 @@ import 'screens/region_selection_screen.dart';
 import 'screens/detail_screen.dart';
 import 'screens/camping_info_screen.dart';
 import 'screens/camping_reservation_screen.dart';
+import 'screens/camping_sites_page.dart';
+import 'screens/search_result_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,10 +19,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/', // ← 초기 화면으로 복구됨!
       routes: {
+
         '/': (context) => RegionSelectionScreen(),
         '/detail': (context) => DetailScreen(),
         '/camping_info': (context) => CampingInfoScreen(),
         '/camping_reservation': (context) => CampingReservationScreen(),
+        '/camping_sites_page': (context)=>CampingSitesPage(),
+        '/search_result': (context)=>SearchResultPage()
       },
     );
   }
